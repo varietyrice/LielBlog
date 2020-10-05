950dfb01e9f70d41efa4fd5b5f4f836d49dfae5c
Guhaprasaanth Nandagopal <guhan.delta@gmail.com>
2020-10-05 19:04:30 -0400

    Created a PageQuery to fetch the blogposts data for the index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,15 +2,54 @@ import React from "react"
 // import { Link } from "gatsby"
 
 import {Layout, SEO, Hero, BlogPostCard} from "components"
+import { graphql } from "gatsby"
 
 
-const IndexPage = () => 
-  <Layout>
-    <SEO title="Home" />
-    <Hero />
-    <main>
-      <BlogPostCard />
-    </main>
-  </Layout>
+const IndexPage = ({data}) => {
+  console.log(data);
+  return(
+    <Layout>
+      <SEO title="Home" />
+      <Hero />
+      <main>
+        <BlogPostCard />
+      </main>
+    </Layout>
+  )
+}
+  
 
 export default IndexPage
+
+// Created a PageQuery using GraphQL to extract the blogposts as it's a dynamic query involving many-
+//- variables to extract the blogpost data
+export const indexPageData = graphql`
+  query BlogListQuery {
+    allMarkdownRemark(
+      filter: {
+        frontmatter: {
+          type: {eq: "post"}
+        }
+      }, 
+        sort: {
+          fields: frontmatter___date, 
+          order: DESC
+        }) {
+      edges {
+        node {
+          fields {
+            readingTime {
+              text
+            }
+          }
+          frontmatter {
+            date
+            title
+          }
+          excerpt
+        }
+      }
+    }
+  }
+
+`
