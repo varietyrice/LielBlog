b5d37e79445611f60819e5cb76945fc2ec9ddff0
Guhaprasaanth Nandagopal <guhan.delta@gmail.com>
2020-10-05 15:35:51 -0400

    Provided the responsive styling for the Hamburger component

--- src/components/Hamburger/index.js
+++ src/components/Hamburger/index.js
@@ -1,9 +1,17 @@
 import React from 'react'
 // Styles
+import {HamburgerIcon} from './Hamburger.styles'
 
 const Hamburger = ({menuOpen, setMenuOpen}) => 
-        <div>
-            <div></div>
-        </div>
+        <HamburgerIcon 
+        role="button" 
+        menuOpen={menuOpen}
+        onClick={() => setMenuOpen(prev => !prev)}
+        >
+            {/* Inline => fn() is used here to pass in some props & prevent the setMenuOpen getting- 
+            -triggered instantly || setter fn() setMenuOpen() has access to previous state, which is-
+            -toggled here */}
+            <div />
+        </HamburgerIcon>
     
 export default Hamburger
