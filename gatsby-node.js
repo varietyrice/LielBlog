fb071e9b2e63cc56c5f379c021b2c6d7200266de
Guhaprasaanth Nandagopal <guhan.delta@gmail.com>
2020-10-05 09:40:32 -0400

    Created a custom hook to retrieve user metadata

--- gatsby-node.js
+++ gatsby-node.js
@@ -6,10 +6,10 @@
  const path = require('path');
  
 // For Absolute Imports
-exports.onCreateWebpackConfig = ({ actions}) =>{
-    actions.setWebpackConfig({
-        resolve: {
-            modules: [path.resolve(__dirname,'src'), 'node_modules']
-        },
-    })
+exports.onCreateWebpackConfig = ({  actions }) => {
+  actions.setWebpackConfig({
+    resolve: {
+      modules: [path.resolve(__dirname, "src"), "node_modules"],
+    },
+  })
 }
