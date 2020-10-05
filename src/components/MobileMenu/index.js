8bd9424ed57bfd6bb08440ccb170ed87f27aaaa0
Guhaprasaanth Nandagopal <guhan.delta@gmail.com>
2020-10-05 16:31:28 -0400

    Provided the responsive styling for the Mobile Menu

--- src/components/MobileMenu/index.js
+++ src/components/MobileMenu/index.js
@@ -1,16 +1,20 @@
 import React from 'react'
 import {Link} from 'gatsby'
+import {OverrideGlobalStyles, Wrapper, Nav} from './MobileMenu.styles'
 
 const MobileMenu = ({menuOpen, items}) => 
-            <div>
-                <ul>
-                    {items.map(item => 
-                        <li key={item.id}>
-                            <Link to={item.link} activeClassName="active">
-                                {item.name}
-                            </Link>
-                        </li> 
-                    )}
-                </ul>
-            </div>
+            <>
+                <OverrideGlobalStyles menuOpen={menuOpen} />
+                <Wrapper menuOpen={menuOpen}>
+                    <Nav>
+                        {items.map(item => 
+                            <li key={item.id}>
+                                <Link to={item.link} activeClassName="active">
+                                    {item.name}
+                                </Link>
+                            </li> 
+                        )}
+                    </Nav>
+                </Wrapper>
+            </>
 export default MobileMenu
