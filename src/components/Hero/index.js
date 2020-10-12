b330983c71fff41059aa9abc60b7e54653632c55
Guhaprasaanth Nandagopal <guhan.delta@gmail.com>
2020-10-12 13:08:44 -0400

    Provided appropriate styling for the Hero Component

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -5,6 +5,7 @@ import BGImage from 'components/BGImage'
 // Hooks
 import {useHeroQuery} from 'hooks/useHeroQuery'
 // Styles
+import { HeroWrapper, TextWrapper } from './Hero.styles'
 
 const Hero = () => {
     const {
@@ -15,18 +16,18 @@ const Hero = () => {
     } = useHeroQuery();
     console.log(heroImage);
     return (
-        <div>
+        <HeroWrapper>
             <BGImage title="heroImage" fluid={heroImage.childImageSharp.fluid}>
-                <div>
+                <TextWrapper>
                     <h2>{heroImageText}</h2>
                     <Link to={heroImageBtnLink}>
                         <button>
                             {heroImageBtnText}
                         </button>
                     </Link>
-                </div>
+                </TextWrapper>
             </BGImage>
-        </div>
+        </HeroWrapper>
     )
 }
 
